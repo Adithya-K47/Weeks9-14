5fd3446b591321f5c1c414ba88110e94e8462912
Adithya Karthik <karthiad@shernet.sheridancollege.ca>
2025-04-08 17:59:00 -0400

    task1 p2

--- Assets/Scripts/ShapeMouseFollow.cs
+++ Assets/Scripts/ShapeMouseFollow.cs
@@ -1,19 +1,32 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShapeMouseFollow : MonoBehaviour
 {
-
+    public SpriteRenderer sr;
+    public float c1;
     // Start is called before the first frame update
     void Start()
     {
+        sr.GetComponent<SpriteRenderer>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        c1 = Random.Range(0, 255);
+        if (Input.GetKeyDown("space"))
+        {
+            sr.color = Random.ColorHSV();
+           
+     
+        }
+
+        
+        
         Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouse.z = 0;
         transform.position = mouse;
